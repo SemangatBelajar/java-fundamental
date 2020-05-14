080e734e9fdcb62e676e1f99a596e70b0a51c89c
SemangatBelajar <r4sauj4ya07@Gmail.com>
2020-05-14 23:47:10 +0700

    menambahkan file BelajarPercabangan.java

---
+++ src/main/java/net/belajar/java/fundamental/percabangan/data/BelajarPercabangan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.belajar.java.fundamental.percabangan.data;
+
+/**
+ *
+ * @author code
+ */
+public class BelajarPercabangan {
+    
+    int uts = 70;
+    int uas = 80;
+}
