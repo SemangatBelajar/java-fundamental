501004d14e0c656be1bbdc36cf5573c1cecd2892
SemangatBelajar <r4sauj4ya07@Gmail.com>
2020-05-14 23:38:40 +0700

    initial commmit

---
+++ src/main/java/net/belajar/java/fundamental/data/App.java
@@ -0,0 +1,13 @@
+package net.belajar.java.fundamental.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/net/belajar/java/fundamental/data/AppTest.java
@@ -0,0 +1,20 @@
+package net.belajar.java.fundamental.data;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
